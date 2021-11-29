00ceee74584d5ad91e099e319c051cff81bc12bc
somaxa8 <silviofrancoxa8@gmail.com>
2021-11-29 13:28:16 -0500

    implemented accessToken verification

--- app/src/main/java/com/somacode/books/controller/SplashActivity.kt
+++ app/src/main/java/com/somacode/books/controller/SplashActivity.kt
@@ -3,10 +3,15 @@ package com.somacode.books.controller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.somacode.books.Main
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, LoginActivity::class.java))
+        Main.session.loginResponse?.token?.accessToken?.let {
+            startActivity(Intent(this, HomeActivity::class.java))
+        } ?:run {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
